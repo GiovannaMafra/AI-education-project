7678bc6368551924392b57f96aeb6627067c0745
Giovanna Mafra <104537611+GiovannaMafra@users.noreply.github.com>
2026-02-28 15:06:20 -0300

    add llm layer and integration

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+from llm.user import call_llm
+
 def main():
     topic = input("Digite o assunto: ")
-    print(topic)
+    result = call_llm(f"Explique de forma simples e breve o que é {topic}.")
+    print("\nResposta:\n")
+    print(result)
 
 if __name__ == "__main__":
     main()
---
+++ llm/user.py
@@ -0,0 +1,68 @@
+import os
+import requests
+from dotenv import load_dotenv
+import time
+
+load_dotenv()
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+print("KEY IN USE:", GEMINI_API_KEY)
+if not GEMINI_API_KEY:
+    raise ValueError("GEMINI_API_KEY not found.")
+
+def call_llm(prompt: str) -> str:
+    url = url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent"
+
+    headers = {
+        "Content-Type": "application/json",
+        "X-goog-api-key": GEMINI_API_KEY,
+    }
+
+    payload = {
+        "contents": [
+            {
+                "parts": [
+                    {"text": prompt}
+                ]
+            }
+        ]
+    }
+    
+
+    max_retries = 1
+    delay_time = 5
+
+    for attempt in range(max_retries):
+        try:
+            response = requests.post(
+                url,
+                headers=headers,
+                json=payload,
+                timeout=30,
+            )
+
+            print("Status code:", response.status_code)
+            print("Response body:", response.text)
+
+            #chave ainda não está funcionando 
+            #está dando limite de requests nessa chave 
+            if response.status_code == 429:
+                print(f"limite atingido")
+                time.sleep(delay_time)
+                continue
+
+            response.raise_for_status()
+            data = response.json()
+
+            text = data["candidates"][0]["content"]["parts"][0]["text"]
+
+            return text.strip()
+
+        except requests.exceptions.RequestException as e:
+            print(f"API request error: {e}")
+            return "Error: Failed to generate content."
+
+        except (KeyError, IndexError):
+            print("Unexpected API response structure.")
+            return "Error: Unexpected API response."
+    return "erro: max retries atingido"
